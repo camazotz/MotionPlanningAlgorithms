e412dfd42b13c5b6f5c081c1f40462249b8dafdd
Nav <nav@dhcp-v880-5aa-00104.1Xwireless.unc.edu>
2016-04-27 04:50:43 -0400

    Modified PRMStar Parameter

--- PRMStar.js
+++ PRMStar.js
@@ -24,11 +24,20 @@ function prm_star (obstacles, x_init, nMilestone, xleft, xright, ybottom, ytop) 
     // r(n) = (log(n) / n)^(1/d), n is number of milestones, d is dimension of space
     var dim = 2;
     //var connRadius = Math.pow((Math.log(nMilestone) / nMilestone), (1/dim));
+    var obstacleSpace = 0;
+    var nextIndex = 0;
+    var numObstacles = obstacles.length / 3;
+    for (var i = 0; i < numObstacles; i++){
+        var obstacle_x = obstacles[nextIndex++];
+        var obstacle_y = obstacles[nextIndex++];
+        var obstacle_r = obstacles[nextIndex++];
+        obstacleSpace += (Math.PI * Math.pow(obstacle_r, 2));
+    }
     var lebesgueVolume = xRange * yRange;
+    lebesgueVolume -= obstacleSpace;
     var unitCircleVolume = Math.PI;
-    //var connRadius = (2 * Math.pow((1+(1/dim)), (1/dim))) * Math.pow((lebesgueVolume/unitCircleVolume),(1/dim));
-
-    var connRadius = 90;
+    var connRadius =  (2 * Math.pow((1+(1/dim)), (1/dim)))*Math.pow((lebesgueVolume/unitCircleVolume),(1/dim));
+    connRadius *= Math.pow(Math.log(nMilestone)/nMilestone, (1/dim));
 
     for (i = 0; i < prmStarGraph.nodeCount(); i++){
         var eachVertex = prmStarGraph.node(prmStarGraph.nodes()[i]);
